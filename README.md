a8b6f88d496eaa78bdd3fee15e321e8a685f42e0
mk-pmb <mk-pmb@users.noreply.github.com>
2017-06-13 00:17:57 +0200

    ignore creds

---
+++ README.md
@@ -0,0 +1,45 @@
+﻿
+<!--#echo json="package.json" key="name" underline="=" -->
+thekwane-pmb
+============
+<!--/#echo -->
+
+<!--#echo json="package.json" key="description" -->
+Helps me filter and sort my email.
+<!--/#echo -->
+
+I used to have Mozilla Thunderbird do it, but increasingly often it flinches
+from acting upon my rules, claiming lots of mail wouldn't match my search
+criteria when I'm pretty sure they do.
+
+Additionally, managing rules accross profiles and computers was becoming
+tedious, so it's time for the Thekwane (lightning bird) to step in.
+
+
+
+Usage
+-----
+
+:TODO:
+
+
+<!--#toc stop="scan" -->
+
+
+
+Known issues
+------------
+
+* needs more/better tests and docs
+
+
+
+
+&nbsp;
+
+
+License
+-------
+<!--#echo json="package.json" key=".license" -->
+ISC
+<!--/#echo -->
---
+++ test/unspam.js
@@ -0,0 +1,55 @@
+﻿/*jslint indent: 2, maxlen: 80, continue: false, unparam: false, node: true */
+/* -*- tab-width: 2 -*- */
+'use strict';
+
+var EX = module.exports, synd = [];
+EX.syncDeciders = synd;
+
+function ifObj(x, d) { return ((x && typeof x) === 'object' ? x : d); }
+
+
+EX.runFromCLI = function () {
+  function guessConfig() {
+    var acc = (process.env.THEKWANE_USER || 'default');
+    if (acc.match(/\/|@/)) { return acc; }
+    return '../logins/' + acc + '.json';
+  }
+  var cfg = require(guessConfig()), tkw = require('thekwane-pmb')(cfg);
+  tkw.on('decideMail', EX.decideMail.bind(tkw));
+  return;
+};
+
+
+EX.decideMail = function (mail, decide) {
+  var decision;
+  EX.syncDeciders.find(function (decider) {
+    decision = ifObj(decider(mail));
+    return !!decision;
+  });
+  return decide(null, decision);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+synd.push(function (mail) { console.log('Unknown mail:', mail.debug()); });
+
+if (require.main === module) { EX.runFromCLI(); }
